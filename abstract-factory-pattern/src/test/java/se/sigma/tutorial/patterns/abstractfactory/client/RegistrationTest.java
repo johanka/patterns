b3d6f3e32ea169f3a020646fcd11b28587d75df5
Johan Karlsson <johanka4@kth.se>
2011-08-30 14:13:09 +0200

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   abstract-factory-pattern/src/test/java/se/sigma/tutorial/patterns/abstractfactory/client/RegistrationTest.java

--- abstract-factory-pattern/src/test/java/se/sigma/tutorial/patterns/abstractfactory/client/RegistrationTest.java
+++ abstract-factory-pattern/src/test/java/se/sigma/tutorial/patterns/abstractfactory/client/RegistrationTest.java
@@ -5,6 +5,8 @@ import se.sigma.tutorial.patterns.abstractfactory.Car;
 import se.sigma.tutorial.patterns.abstractfactory.CarFactory;
 import se.sigma.tutorial.patterns.abstractfactory.impl.HeavyCar;
 import se.sigma.tutorial.patterns.abstractfactory.impl.HeavyCarFactory;
+import se.sigma.tutorial.patterns.abstractfactory.impl.LightCar;
+import se.sigma.tutorial.patterns.abstractfactory.impl.LightCarFactory;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -18,6 +20,18 @@ public class RegistrationTest {
         Registration registration = new Registration();
 
         registration.setCarFactory(heavyCarFactory);
+        Car actualCar = registration.registerNewCar();
+
+        assertThat(actualCar, is(expectedCar.getClass()));
+    }
+
+    @Test
+    public void testCreateLightCar() {
+        Car expectedCar = new LightCar();
+        CarFactory lightCarFactory = new LightCarFactory();
+        Registration registration = new Registration();
+
+        registration.setCarFactory(lightCarFactory);
         Car actualCar = registration.registerNewCar();
 
         assertThat(actualCar, is(expectedCar.getClass()));
