0c3d1758674cdc1376d017b2b22e1e0506307a67
Johan Karlsson <johanka4@kth.se>
2011-08-31 14:04:01 +0200

    added budsbuilder

---
+++ builder-pattern-transport/src/main/java/se/sigma/tutorial/patterns/builder/builders/BusBuilder.java
@@ -0,0 +1,23 @@
+package se.sigma.tutorial.patterns.builder.builders;
+
+public class BusBuilder extends AbstractTransportBuilder {
+    private static final String TYPE = "Bus";
+    private static final String MODEL = "Volvo B58";
+    private static final String TIME_PLAN = "Sqrubben - Trondheim";
+
+
+    @Override
+    public void buildType() {
+        transport.setType(TYPE);
+    }
+
+    @Override
+    public void buildModel() {
+        transport.setModel(MODEL);
+    }
+
+    @Override
+    public void buildTimePlan() {
+        transport.setTimePlan(TIME_PLAN);
+    }
+}
---
+++ builder-pattern-transport/src/test/java/se/sigma/tutorial/patterns/builder/builders/BusBuilderTest.java
@@ -0,0 +1,32 @@
+package se.sigma.tutorial.patterns.builder.builders;
+
+import org.junit.Test;
+import se.sigma.tutorial.patterns.builder.Transport;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BusBuilderTest {
+
+    private static final String TYPE = "Bus";
+    private static final String MODEL = "Volvo B58";
+    private static final String TIME_PLAN = "Sqrubben - Trondheim";
+
+    @Test
+    public void testBuildVolvoBus() {
+        AbstractTransportBuilder busBuilder = new BusBuilder();
+        Transport transport = buildBus(busBuilder);
+
+        assertThat(transport.getType(), is(TYPE));
+        assertThat(transport.getModel(), is(MODEL));
+        assertThat(transport.getTimePlan(), is(TIME_PLAN));
+    }
+
+    private Transport buildBus(AbstractTransportBuilder busBuilder) {
+        busBuilder.createNewTransport();
+        busBuilder.buildType();
+        busBuilder.buildModel();
+        busBuilder.buildTimePlan();
+        return busBuilder.getTransport();
+    }
+}
