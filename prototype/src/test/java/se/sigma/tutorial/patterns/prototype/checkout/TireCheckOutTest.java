b977785900edd0c809647126d0ba44e39d4c1a63
Johan <johanka4@kth.se>
2011-09-23 15:22:30 +0200

    begining with PrototypeManager for tires

---
+++ prototype/src/test/java/se/sigma/tutorial/patterns/prototype/checkout/TireCheckOutTest.java
@@ -0,0 +1,16 @@
+package se.sigma.tutorial.patterns.prototype.checkout;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class TireCheckOutTest {
+    // TODO
+
+    @Test
+    @Ignore("todo: implement")
+    public void askForWinterTire() {
+        fail("to be implemented");
+    }
+}
