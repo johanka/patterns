d65e74fead4b547b71c4ea5d7d7818b4ed0894c6
unknown <Koppen@.(none)>
2011-09-24 21:34:37 +0200

    small housekeeping

--- abstract-factory-pattern-car/src/test/java/se/sigma/tutorial/patterns/abstractfactory/client/RegistrationTest.java
+++ abstract-factory-pattern-car/src/test/java/se/sigma/tutorial/patterns/abstractfactory/client/RegistrationTest.java
@@ -1,5 +1,6 @@
 package se.sigma.tutorial.patterns.abstractfactory.client;
 
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
 import se.sigma.tutorial.patterns.abstractfactory.Car;
 import se.sigma.tutorial.patterns.abstractfactory.CarFactory;
@@ -9,31 +10,30 @@ import se.sigma.tutorial.patterns.abstractfactory.impl.LightCar;
 import se.sigma.tutorial.patterns.abstractfactory.impl.LightCarFactory;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertThat;
 
 public class RegistrationTest {
 
     @Test
     public void testCreateHeavyCar() {
-        Car expectedCar = new HeavyCar();
         CarFactory heavyCarFactory = new HeavyCarFactory();
         Registration registration = new Registration();
-
         registration.setCarFactory(heavyCarFactory);
+
         Car actualCar = registration.registerNewCar();
 
-        assertThat(actualCar, is(expectedCar.getClass()));
+        assertThat(actualCar, instanceOf(HeavyCar.class));
     }
 
     @Test
     public void testCreateLightCar() {
-        Car expectedCar = new LightCar();
         CarFactory lightCarFactory = new LightCarFactory();
         Registration registration = new Registration();
-
         registration.setCarFactory(lightCarFactory);
+
         Car actualCar = registration.registerNewCar();
 
-        assertThat(actualCar, is(expectedCar.getClass()));
+        assertThat(actualCar, is(LightCar.class));
     }
 }
